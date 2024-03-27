912622ae8328d49ef85cb975e166c4f90a2b4bda
Xuanzhao Gao <gaoxuanzhao@gmail.com>
2024-03-27 10:45:32 +0800

    revised doc

--- src/grid.jl
+++ src/grid.jl
@@ -10,7 +10,7 @@ function GridInfo(N_real::NTuple{N, Int}, w::NTuple{N, Int}, periodicity::NTuple
     L: the length of the box in each direction
     h: the grid spacing in each direction, the first point is at [h/2, 3h/2, ..., L - h/2]
     N_image: the number of grid points in each direction for the image grid, given by tuple([2 * w[i] + 2 for i in 1:N]...) .+ N_real
-    N_pad: the numer of padded grid points in each direction, given by tuple([2 * pad[i] * (periodicity[i] == false) for i in 1:N]...) .+ N_image .+ N_real
+    N_pad: the numer of padded grid points in each direction, given by tuple([2 * pad[i] * (periodicity[i] == false) for i in 1:N]...) .+ N_real
 """
 function GridInfo(N_real::NTuple{N, Int}, w::NTuple{N, Int}, periodicity::NTuple{N, Bool}, extra_pad::NTuple{N, Int}, L::NTuple{N, T}) where{N, T<:Union{Float32, Float64}}
     N_image = tuple([2 * w[i] + 2 for i in 1:N]...) .+ N_real
